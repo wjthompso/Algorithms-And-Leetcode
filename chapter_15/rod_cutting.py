a34e5474afd59850c49b2970b83d0f3388afb94a
wjthompso <49210425+wjthompso@users.noreply.github.com>
2023-12-12 15:01:24 -0800

    Adding Chapter 15.1 rod cutting code

--- chapter_15/rod_cutting.py
+++ chapter_15/rod_cutting.py
@@ -50,11 +50,37 @@ def memoized_cut_rod_bottom_up(p: list, n: int) -> int:
     return r[n]
 
 
+# Extended bottom-up approach: Showing the length of the optimal cuts
+
+
+def extended_bottom_up_cut_rod(p: list, n: int) -> tuple:
+    """Returns the maximum revenue for a rod of length n."""
+    r = [-1 for _ in range(n + 1)]
+    s = [-1 for _ in range(n + 1)]
+    r[0] = 0
+    for j in range(1, n + 1):
+        q = -1
+        for i in range(1, j + 1):
+            if q < p[i] + r[j - i]:
+                q = p[i] + r[j - i]
+                s[j] = i
+        r[j] = q
+    return r, s
+
+
+def print_cut_rod_solution(p: list, n: int) -> None:
+    r, s = extended_bottom_up_cut_rod(p, n)
+    while n > 0:
+        print(s[n])
+        n = n - s[n]
+
+
 class TestRodCuttingAlgorithms(unittest.TestCase):
     def run_all_tests(self) -> None:
         self.test_cut_rod()
         self.test_top_down_memoized_cut_rod()
         self.test_bottom_up_cut_rod()
+        self.test_extended_bottom_up_cut()
 
     def test_cut_rod(self) -> None:
         p = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
@@ -95,6 +121,34 @@ class TestRodCuttingAlgorithms(unittest.TestCase):
         self.assertEqual(memoized_cut_rod_bottom_up(p, 9), 25)
         self.assertEqual(memoized_cut_rod_bottom_up(p, 10), 30)
 
+    def test_extended_bottom_up_cut(self) -> None:
+        p = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+        r, s = extended_bottom_up_cut_rod(p, 1)
+        self.assertEqual(r, [0, 1])
+        self.assertEqual(s, [-1, 1])
+        r, s = extended_bottom_up_cut_rod(p, 2)
+        self.assertEqual(r, [0, 1, 5])
+        self.assertEqual(s, [-1, 1, 2])
+        r, s = extended_bottom_up_cut_rod(p, 3)
+        self.assertEqual(r, [0, 1, 5, 8])
+        self.assertEqual(s, [-1, 1, 2, 3])
+        r, s = extended_bottom_up_cut_rod(p, 4)
+        self.assertEqual(r, [0, 1, 5, 8, 10])
+        self.assertEqual(s, [-1, 1, 2, 3, 2])
+        r, s = extended_bottom_up_cut_rod(p, 5)
+        self.assertEqual(r, [0, 1, 5, 8, 10, 13])
+        self.assertEqual(s, [-1, 1, 2, 3, 2, 2])
+        r, s = extended_bottom_up_cut_rod(p, 6)
+        self.assertEqual(r, [0, 1, 5, 8, 10, 13, 17])
+        self.assertEqual(s, [-1, 1, 2, 3, 2, 2, 6])
+        r, s = extended_bottom_up_cut_rod(p, 7)
+        self.assertEqual(r, [0, 1, 5, 8, 10, 13, 17, 18])
+        self.assertEqual(s, [-1, 1, 2, 3, 2, 2, 6, 1])
+
 
 if __name__ == "__main__":
     TestRodCuttingAlgorithms().run_all_tests()
+
+    r, s = extended_bottom_up_cut_rod([0, 1, 5, 8, 10, 13, 17, 18, 22, 25, 30], 9)
+
+    print_cut_rod_solution([0, 1, 5, 8, 10, 13, 17, 18, 22, 25, 30, 0, 0, 0, 0, 0], 14)
